63bd14b27c6fd94f4742acb59d5b12fdad32b94b
soongon <soongon@gmail.com>
2024-08-20 12:11:32 +0900

    무비데이터페칭-메인페이지 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
 
-export default function Home() {
+// next14에서 작성한 모든 컴포넌트는 (App 디렉토리 내의 컴포넌트) 서버 컴포넌트임
+export default async function Home() {
+
+  //1. imdb에서 데이터 페칭 수행 --> movie 데이터 json 파일 응답
+  //2. json.results --> []이 응답.. 20개의 영화 데이터를 포함
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDM3YTAwN2ZhMmE5MzM1NTdmNWYyMzBlMGYyZTYwZiIsIm5iZiI6MTcyNDEyMTgzNS45Mzg1OTEsInN1YiI6IjY0OTkzOGVkNmY0M2VjMDBjNWM3MmY4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lKLi0NyV3qV0XNsDaZbeHw6IWrAjDam7TWu0nq9wxnc'
+    }
+  };
+  
+  const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1&region=kr', options)
+  const json = await response.json();
+  const movies = json.results;  // 데이터 확보
+
   return (
     <div>
-      <h1>Hello Next</h1>
+      {
+        movies.map(movie => {
+          return <div>{movie.title}</div>
+        })
+      }
     </div>
   );
 }
