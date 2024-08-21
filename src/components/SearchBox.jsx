bba2fa79a3380084b4a41f299bf22131b4126f29
soongon <soongon@gmail.com>
2024-08-21 10:29:44 +0900

    searchBox 구현

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,18 +1,24 @@
 "use client"
 
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 
 export default function SearchBox() {
 
     const [searchTerm, setSearchTerm] = useState('');
+    const router = useRouter();
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
+        //  /serach/[searchTerm] 경로로 이동
+        //window.location.href = '/search/'+searchTerm;
+        router.push(`/search/${searchTerm}`);
+        console.log(searchTerm + '로 검색합니다.');
     };
 
     const onChangeHandler = (e) => {
         setSearchTerm(e.target.value);
-        console.log(searchTerm);
+        //console.log(searchTerm);
     }
 
     return (
@@ -25,7 +31,8 @@ export default function SearchBox() {
                 className='w-full h-14 rounded outline-none bg-transparent flex-1 text-xl'
                 placeholder='검색어를 입력해주세요'/>
             <button
-                className='text-amber-600 text-xl'>검색</button>
+                className='text-amber-600 text-xl hover:text-amber-800'
+                disabled={searchTerm === ''}>검색</button>
         </form>
     )
 }
